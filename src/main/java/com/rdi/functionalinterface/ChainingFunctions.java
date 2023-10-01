b2fc4190c007299f5e63323e138c555b3da87570
rdayok <r.dayok@native.semicolon.africa>
2023-10-01 11:55:29 +0100

    Chaining functions in java

--- src/main/java/com/rdi/functionalinterface/ChainingFunctions.java
+++ src/main/java/com/rdi/functionalinterface/ChainingFunctions.java
@@ -2,24 +2,23 @@ package com.rdi.functionalinterface;
 
 import java.util.function.Function;
 
-public class _Fuction {
+public class ChainingFunctions {
     public static void main(String[] args) {
         int increment = increment(0);
         System.out.println(increment);
-        int result = incrementByOneFunction.apply(5);
-        System.out.println(result);
-        // Note that the two functions below are no different but one is in a functional way
+        // Say we want to combine increment and multipleByFive functions to get a desired state
+        Function<Integer, Integer> incrementAndMultiplyByFive = incrementByOneFunction.andThen(multiplyByFive);
+        System.out.println(incrementAndMultiplyByFive.apply(1));
     }
 
 
     // The first Integer and second Integer the Function takes rep the
     // input and the return type respectively
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+    static Function<Integer, Integer> multiplyByFive = number -> number * 5;
 
     public static int increment(int number) {
         return number + 1;
     }
 
-
 }
-
